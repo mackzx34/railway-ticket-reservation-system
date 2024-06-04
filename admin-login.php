0b96ca553d2bc3f3a34f59332d947c0fe1056ffb
unknown <Deleter@DELETERALI.cyber.net>
2024-06-04 06:35:43 +0300

    Second Release of Admin Login Page Changes

--- admin-login.php
+++ admin-login.php
@@ -3,9 +3,8 @@
   include "include/connection.inc.php";
   DatabaseConnect();
   $errors = array('authenticate' => '');
-  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-  if(isset($_POST['signin'])){
+  if(isset($_POST['signin']) && isset ($_POST['username']) && isset ($_POST['password'])){
     // Anti-CSRF
     if (array_key_exists ("session_token", $_SESSION)) {
       $session_token = $_SESSION[ 'session_token' ];
@@ -15,42 +14,100 @@
 
     checkToken( $_REQUEST[ 'user_token' ], $session_token, 'admin-login.php' );
 
+    // Sanitise username input
     $user = $_POST[ 'username' ];
 	  $user = stripslashes( $user );
     $user = ((isset($GLOBALS["___conn"]) && is_object($GLOBALS["___conn"])) ? mysqli_real_escape_string($GLOBALS["___conn"],  $user ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
 
+    // Sanitise password input
     $pass = $_POST[ 'password' ];
     $pass = stripslashes( $pass );
     $pass = ((isset($GLOBALS["___conn"]) && is_object($GLOBALS["___conn"])) ? mysqli_real_escape_string($GLOBALS["___conn"],  $pass ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
     $pass = md5( $pass );
 
-        
-        if(! array_filter($errors)){
-            $username = $conn->real_escape_string($username);
-            $password = $conn->real_escape_string($password);
-            
-            //CHECK CORRECT USERNAME PASSWORD
-            $query = ("SELECT * FROM user WHERE username = '$user' AND password = '$pass' LIMIT 1");
-            $result = @mysqli_query($GLOBALS["___conn"],  $query );
-            if( mysqli_num_rows( $result ) != 1 ) {
-              $errors['authenticate'] = 'Incorrect username or password';
-            }
+    // Default values
+    $total_failed_login = 3;
+    $lockout_time       = 15;
+    $account_locked     = false;
 
-            if( $result && mysqli_num_rows( $result ) == 1 ) {
-              $_SESSION['username'] = $user;
-              $row = mysqli_fetch_assoc($result);
-              
-                if($row["role"] == TRUE){
-                  header('Location: admin-page.php');
-                } else {
-                  header('Location: user.php');
-                }
+    // Check the database (Check user information)
+    $data = $db->prepare( 'SELECT failed_login, last_login FROM users WHERE user = (:user) LIMIT 1;' );
+    $data->bindParam( ':user', $user, PDO::PARAM_STR );
+    $data->execute();
+    $row = $data->fetch();
 
-            }
+    // Check to see if the user has been locked out.
+    if( ( $data->rowCount() == 1 ) && ( $row[ 'failed_login' ] >= $total_failed_login ) )  {
+      // User locked out.  Note, using this method would allow for user enumeration!
+      //$html .= "<pre><br />This account has been locked due to too many incorrect logins.</pre>";
 
-            $conn->close();
-        }
+      // Calculate when the user would be allowed to login again
+      $last_login = strtotime( $row[ 'last_login' ] );
+      $timeout    = $last_login + ($lockout_time * 60);
+      $timenow    = time();
+
+
+      // Check to see if enough time has passed, if it hasn't locked the account
+      if( $timenow < $timeout ) {
+        $account_locked = true;
+        // print "The account is locked<br />";
+      }
     }
+
+    // Check the database (if username matches the password)
+    $data = $db->prepare( 'SELECT * FROM users WHERE user = (:user) AND password = (:password) LIMIT 1;' );
+    $data->bindParam( ':user', $user, PDO::PARAM_STR);
+    $data->bindParam( ':password', $pass, PDO::PARAM_STR );
+    $data->execute();
+    $row = $data->fetch();
+
+    // If its a valid login...
+    if( ( $data->rowCount() == 1 ) && ( $account_locked == false ) ) {
+      // Get users details
+      // $avatar       = $row[ 'avatar' ];
+      $failed_login = $row[ 'failed_login' ];
+      $last_login   = $row[ 'last_login' ];
+      $user         = $row[ 'user'];
+      $role         = $row[ 'role'];
+
+      // Login successful
+      $_SESSION['username'] = $user;
+      if($role == TRUE){
+        header('Location: admin-page.php');
+      } else {
+        header('Location: user.php');
+      }
+      
+
+      // Had the account been locked out since last login?
+      // if( $failed_login >= $total_failed_login ) {
+      //   $errors['authenticate']  .= "<p><em>Warning</em>: Someone might of been brute forcing your account.</p>";
+      //   $errors['authenticate']  .= "<p>Number of login attempts: <em>{$failed_login}</em>.<br />Last login attempt was at: <em>{$last_login}</em>.</p>";
+      // }
+
+      // Reset bad login count
+      $data = $db->prepare( 'UPDATE users SET failed_login = "0" WHERE user = (:user) LIMIT 1;' );
+      $data->bindParam( ':user', $user, PDO::PARAM_STR );
+      $data->execute();
+    } else {
+      // Login failed
+      sleep( rand( 2, 4 ) );
+
+      // Give the user some feedback
+      $errors['authenticate'] = "Incorrect username or password <em>please try again in {$lockout_time} minutes</em>";
+      // $html .= "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, <em>please try again in {$lockout_time} minutes</em>.</pre>";
+
+      // Update bad login count
+      $data = $db->prepare( 'UPDATE users SET failed_login = (failed_login + 1) WHERE user = (:user) LIMIT 1;' );
+      $data->bindParam( ':user', $user, PDO::PARAM_STR );
+      $data->execute();
+    }
+
+    // Set the last login time
+    $data = $db->prepare( 'UPDATE users SET last_login = now() WHERE user = (:user) LIMIT 1;' );
+    $data->bindParam( ':user', $user, PDO::PARAM_STR );
+    $data->execute();
+  }
 
     Header( 'Cache-Control: no-cache, must-revalidate');    // HTTP/1.1
     Header( 'Content-Type: text/html;charset=utf-8' );      // TODO- proper XHTML headers...
